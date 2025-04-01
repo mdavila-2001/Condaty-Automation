a16688abc8c9fe4227fc604fbb9a9b3f310030c7
Marcelo Dávila <marcelo.tester.fos@gmail.com>
2025-04-01 09:37:52 -0400

    test(areasTest): add test cases for listing, updating, and deleting areas
    This commit introduces new test cases to verify the functionality of listing, updating, and deleting areas. The tests ensure proper API interaction and validate the responses for each operation.

--- areas/areasTest.py
+++ areas/areasTest.py
@@ -11,7 +11,7 @@ URL_BASE = "https://phplaravel-1214481-5270819.cloudwaysapps.com/api"
 #Datos necesarios
 LISTADO = {"fullType": "L"}
 AREA_A_CREAR = {
-    "title": fake.place_name(),
+    "title": fake.name(),
     "description": fake.sentence(),
     "max_capacity": 50,
     "days": "0111110",
@@ -34,3 +34,55 @@ def obtenerToken():
     response.raise_for_status()
     datos = response.json()
     return datos['data']['token']
+
+def test_listar_area():
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.get(f"{URL_BASE}/areas", headers=headers, params=LISTADO)
+        response.raise_for_status()
+        datos = response.json()
+        assert "message" in datos, "La respuesta no contiene la clave 'message'"
+        print("El GET funciona correctamente")
+        print(json.dumps(datos['data'], indent=4))
+    except requests.exceptions.HTTPError as e:
+        pytest.fail(f"listar_usuarios: Prueba fallida - {e}")
+
+@pytest.fixture(scope="module")
+def crear_area():
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.post(f"{URL_BASE}/areas", headers=headers, json=AREA_A_CREAR)
+        response.raise_for_status()
+        datos = response.json()
+        assert datos['message'] == "Registro creado con éxito", "El area falló al crearse"
+        print(f"Area creada con ID: {datos['data']}")
+        return datos['data']
+    except requests.exceptions.HTTPError as e:
+        pytest.fail(f"crear_area: Prueba fallida - {e}")
+
+def test_actualizar_area(crear_area):
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.put(f"{URL_BASE}/areas/{crear_area}", headers=headers, json=AREA_A_EDITAR)
+        response.raise_for_status()
+        datos = response.json()
+        assert datos['message'] == "Registro actualizado con éxito", "El area falló al actualizarse"
+        print(json.dumps(datos['data'], indent=4))
+        print(f"Area actualizada con ID: {crear_area}")
+    except requests.exceptions.HTTPError as e:
+        pytest.fail(f"actualizar_area: Prueba fallida - {e}")
+
+def test_eliminar_area(crear_area):
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.delete(f"{URL_BASE}/areas/{crear_area}", headers=headers)
+        response.raise_for_status()
+        datos = response.json()
+        assert datos['message'] == "Registro eliminado con éxito", "El area falló al eliminarse"
+        print(f"Area eliminada con ID: {crear_area}")
+    except requests.exceptions.HTTPError as e:
+        pytest.fail(f"eliminar_area: Prueba fallida - {e}")
